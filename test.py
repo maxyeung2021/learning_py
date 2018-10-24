7b0b11e53bf498e07e38baa3edc962930733b2cd
mingxinyang <492986751@qq.com>
2018-10-24 23:09:40 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html', name=name)
+    #return 'Hello World!'
+
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run()
